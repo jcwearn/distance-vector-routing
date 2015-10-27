011c617200198ddb569f07bc8fc3d763e723f0e7
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-27 18:22:38 -0400

    Add skeleton code for dvector class

---
+++ dvector.py
@@ -0,0 +1,10 @@
+class DVector():
+    def __init__(self, nodeName):
+
+    def destinations(self):
+
+    def distance(self, destinations):
+
+    def distance(self, destination):
+
+    def louadRoute(self, destination, dist):
