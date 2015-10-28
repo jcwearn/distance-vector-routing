a65f4a57fce6fa96d33746aeb2691d2adf8ea968
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-28 13:04:02 -0400

    Add method to calculare first hops, change variable names

--- bellmanford.py
+++ bellmanford.py
@@ -1,26 +1,40 @@
 def initialize(graph, source):
-    d = {} 
-    p = {} 
+    distance = {} 
+    predecessor = {}
     for node in graph:
-        d[node] = 16
-        p[node] = None
-    d[source] = 0 
-    return d, p
+        '''initialize the distance and predecessor dictionaries'''
+        distance[node] = 16
+        predecessor[node] = None
+    distance[source] = 0 
+    return distance, predecessor
 
-def relax(node, neighbour, graph, d, p):
-    if d[neighbour] > d[node] + graph[node][neighbour]:
-        d[neighbour]  = d[node] + graph[node][neighbour]
-        p[neighbour] = node
+def relax(node, neighbor, graph, distance, predecessor):
+    if distance[neighbor] > distance[node] + graph[node][neighbor]:
+        distance[neighbor]  = distance[node] + graph[node][neighbor]
+        predecessor[neighbor] = node
 
-def bellmanFord(graph, source):
-    d, p = initialize(graph, source)
-    for i in range(len(graph)-1): 
+def calcFirstHop(distance, predecessor, source, neighbors):
+    firstHop = predecessor.copy()
+    for key, value in firstHop.iteritems():
+        if key == source:
+            firstHop[key] = key
+        elif value == source and key != source:
+            firstHop[key] = key
+        elif key not in neighbors:
+            if value not in neighbors:
+                firstHop[key] = firstHop[value]
+
+    return firstHop
+
+def bellmanFord(graph, source, neighbors):
+    distance, predecessor = initialize(graph, source)
+    for i in range(len(graph)-1):
+        '''iterate n times where n is the number of nodes in the graph'''
         for u in graph:
-            for v in graph[u]: 
-                relax(u, v, graph, d, p) 
+            '''for each of the above iterations, also iterate through each node in the graph'''
+            for v in graph[u]:
+                '''relax graph for each neighbor node'''
+                relax(u, v, graph, distance, predecessor) 
 
-    for u in graph:
-        for v in graph[u]:
-            assert d[v] <= d[u] + graph[u][v]
-
-    return d, p
+    firstHop = calcFirstHop(distance, predecessor, source, neighbors)
+    return distance, firstHop
