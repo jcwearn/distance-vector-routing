b09e7c4861ed7d0664efeec352e10922f0f16e78
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-28 14:51:17 -0400

    Replace getRoutes()  with destionations()

--- dvectortest.py
+++ dvectortest.py
@@ -49,30 +49,30 @@ class TestDVector(unittest.TestCase):
         dv = dvector.DVector('A')
 
         dv.loadRoute('B', 4.5)
-        self.assertIn('B', dv.getRoutes())
+        self.assertIn('B', dv.destinations())
         self.assertEqual(dv.distance('B'), 4.5)
 
         dv.loadRoute('C', 5.5)
-        self.assertIn('C', dv.getRoutes())
+        self.assertIn('C', dv.destinations())
         self.assertEqual(dv.distance('C'), 5.5)
 
         dv.loadRoute('D', 2)
-        self.assertIn('D', dv.getRoutes())
+        self.assertIn('D', dv.destinations())
         self.assertEqual(dv.distance('D'), 2)
 
     def test_removeRoute(self):
         '''test removeRoute method which removes a route 'destination' from a routing table '''
         dv = dvector.DVector('A')
         dv.loadRoute('B', 4.5)
-        self.assertIn('B', dv.getRoutes())
+        self.assertIn('B', dv.destinations())
         dv.removeRoute('B')
-        self.assertNotIn('B', dv.getRoutes())
+        self.assertNotIn('B', dv.destinations())
 
         dv = dvector.DVector('B')
         dv.loadRoute('C', 4)
-        self.assertIn('C', dv.getRoutes())
+        self.assertIn('C', dv.destinations())
         dv.removeRoute('C')
-        self.assertNotIn('C', dv.getRoutes())
+        self.assertNotIn('C', dv.destinations())
 
 if __name__ == '__main__':
     unittest.main()
