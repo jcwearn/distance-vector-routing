5a2d5b2f51e594fb4289c10f5721c56614f30c40
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-27 20:14:55 -0400

    Test code for DVRouter constructor

--- dvroutertest.py
+++ dvroutertest.py
@@ -3,7 +3,17 @@ import dvrouter
 
 class TestDVRouter(unittest.TestCase):
     def test_constructor(self):
-        '''Test that a newly created DVRouter has a link to itself byt to no other node'''
+        '''Test that a newly created DVRouter has a link to itself but to no other node'''
+        router1 = dvrouter.DVRouter('A')
+        table1 = router1.getRoutingTable()
+        self.assertEqual(len(table1), 1)
+        self.assertEqual(table1['A'], 0)
+
+        router2 = dvrouter.DVRouter('B')
+        table2 = router2.getRoutingTable()
+        self.assertEqual(len(table2), 1)
+        self.assertEqual(table2['B'], 0)
+        
     def test_addLink(self):
         '''Test adding a link to a neighbor'''
     def test_removeLink(self):
@@ -12,7 +22,7 @@ class TestDVRouter(unittest.TestCase):
         '''Test exporting distance vectors to a neighbor. If split horizon is on, should not advertize a route whose first hop is some neighbor to that neighbor'''
     def test_importDistanceVectors(self):
         '''Test importing a list of distance vectors.  It should raise a KeyError if the vector does not come from a neighbor'''
-    def test_recomputeTable(self):
-        '''Test recomputing the routing table via the Bellman-Ford algorithm'''
+    def test_updateRoutingTable(self):
+        '''Test updating the routing table via the Bellman-Ford algorithm'''
 if __name__ == '__main__':
     unittest.main()        
