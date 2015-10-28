ad375ab477c90a9d3079aff5f1eefa30a60c5263
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-28 02:41:01 -0400

    Put bellmanford code in another file. Start implementation of updateRoutingTable

--- dvrouter.py
+++ dvrouter.py
@@ -1,4 +1,5 @@
 import dvector
+from bellmanford import bellmanFord
 
 class DVRouter():
     def __init__(self, routerName):
@@ -27,34 +28,9 @@ class DVRouter():
         self.importedTables = neighborTables
 
     def updateRoutingTable(self):
-        pass
-
-    def initialize(graph, source):
-        d = {} 
-        p = {} 
-        for node in graph:
-            d[node] = 16
-            p[node] = None
-        d[source] = 0 
-        return d, p
-
-    def relax(node, neighbour, graph, d, p):
-        if d[neighbour] > d[node] + graph[node][neighbour]:
-            d[neighbour]  = d[node] + graph[node][neighbour]
-            p[neighbour] = node
-
-    def bellmanFord(graph, source):
-        d, p = initialize(graph, source)
-        for i in range(len(graph)-1): 
-            for u in graph:
-                for v in graph[u]: 
-                    relax(u, v, graph, d, p) 
-
-        for u in graph:
-            for v in graph[u]:
-                assert d[v] <= d[u] + graph[u][v]
-
-        return d, p
+        d, p = bellmanFord(self.getImportedTables(), self.getRouterName())
+        print d
+        print p
 
     def getRouterName(self):
         return self.routerName
