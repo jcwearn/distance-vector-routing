6503bd7a6e2670b1daef28545b641825f086c85e
Jackson Wearn <jackson.wearn@careerbuilder.com>
2015-10-28 14:42:04 -0400

    Add docstring comments

--- bellmanford.py
+++ bellmanford.py
@@ -1,4 +1,5 @@
 def initialize(graph, source):
+    '''Initializes the distance and predecessor dictionaries for the Bellman-Ford algorithm.  This method takes in a graph of weighted nodes, and a source node from the graph.  It returns a distance and predecessor dictionary'''
     distance = {} 
     predecessor = {}
     for node in graph:
@@ -9,11 +10,13 @@ def initialize(graph, source):
     return distance, predecessor
 
 def relax(node, neighbor, graph, distance, predecessor):
+    '''Relaxes the distance value for a given node in a graph.  It updates the distance and predecessor dictionary if the distance between the node and it's neighbor is smaller than what is already held in the distance dictionary.  This function takes in a node, it's neighbor node, the full graph, the distance dictrionary, and the predecessor dictionary'''
     if distance[neighbor] > distance[node] + graph[node][neighbor]:
         distance[neighbor]  = distance[node] + graph[node][neighbor]
         predecessor[neighbor] = node
 
 def calcFirstHop(predecessor, source, neighbors):
+    '''Calculates the firstHop to each node from a source node.  It uses the predecessor dictionary that is returned from the Bellman-Ford algorithm.  It takes in the predecessor dictionary, the source node, and the source nodes neighbors.  This function returns a dictionary containing the first hops to all other nodes from the source node.'''
     firstHop = predecessor.copy()
     for key, value in firstHop.iteritems():
         '''set correct first hop for each key based on the predecessor'''
@@ -28,6 +31,7 @@ def calcFirstHop(predecessor, source, neighbors):
     return firstHop
 
 def bellmanFord(graph, source, neighbors):
+    '''Runs the Bellman-Ford algorithm on a graph given it's source and neighbors.  It takes in a graph, the source node to start the algorithm from, and the neighbors of the source node.  It returns a distance dictionary containing the shortest distances to all other nodes and a firstHop dictionary which contains the first hop from the source node to all other nodes'''
     distance, predecessor = initialize(graph, source)
     for i in range(len(graph)-1):
         '''iterate n times where n is the number of nodes in the graph'''
